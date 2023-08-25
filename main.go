fbe1ef542a07731fab649a77179891f2c6f6aae9
ArZhi <zhi711825@gmail.com>
2023-08-25 11:32:35 +0800

    update service

--- main.go
+++ main.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func test(c *gin.Context) {
-	c.String(http.StatusOK, "hello")
+func index(c *gin.Context) {
+	t := time.Now().UTC().Add(time.Hour * 8)
+	txt := "Ping" + t.String()
+	c.String(http.StatusOK, txt)
+	wlog(txt)
 }
 
 func runGin() {
 	server := gin.Default()
-	server.GET("/", test)
+	server.GET("/", index)
 	server.Run(":8888")
 }
 
-func main() {
-	txt := "HELLO_WORLD_To_小蔡!!"
+func wlog(txt string) {
 
-	fmt.Println(txt)
-
+	log.Println(txt)
 	err1 := os.WriteFile("/tmp/data.txt", []byte(txt), 777)
 	if err1 != nil {
 		log.Fatal(err1)
 	}
+}
+
+func main() {
+	runGin()
 }
