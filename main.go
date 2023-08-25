db8082c97cb1e8701764ef697116cc70a04d3115
ArZhi <zhi711825@gmail.com>
2023-08-25 10:37:44 +0800

    change log path

--- main.go
+++ main.go
@@ -24,8 +24,13 @@ func main() {
 
 	fmt.Println(txt)
 
-	err := os.WriteFile("data.txt", []byte(txt), 777)
-	if err != nil {
-		log.Fatal(err)
+	err1 := os.WriteFile("/tmp/data.txt", []byte(txt), 777)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	err2 := os.WriteFile("/var/data.txt", []byte(txt), 777)
+	if err2 != nil {
+		log.Fatal(err2)
 	}
 }
