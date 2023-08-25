43dd007b8fad3dce59315cfd21675403d2587412
ArZhi <zhi711825@gmail.com>
2023-08-25 10:14:02 +0800

    run fmt

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func test(c *gin.Context) {
-	c.String(http.StatusOK, "hello_8/25")
+	c.String(http.StatusOK, "hello")
 }
-func main() {
-	// test1
+
+func runGin() {
 	server := gin.Default()
 	server.GET("/", test)
 	server.Run(":8888")
+}
+
+func main() {
+	fmt.Println("HELLO_WORLD!!")
 }
