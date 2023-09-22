502db7614efa5178b597d47f91a6fb927d1620aa
ArZhi <zhi711825@gmail.com>
2023-09-22 12:13:02 +0800

    update sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker-compose up -d
+if [ -n "$(docker ps -q -f name=<container_name>)" ]; then
+    echo "go-server容器存在"
+    docker stop go-server
+    docker rm go-server
+else
+    docker build -t demo . --no-cache
+    docker run -p 8088:8088 --name go-server -d demo
+fi
