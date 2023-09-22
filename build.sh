430e267916b8191c2fe266cf0c3d95700a52ed28
ArZhi <zhi711825@gmail.com>
2023-09-22 12:14:54 +0800

    update sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -n "$(docker ps -q -f name=go-server)" ]; then
-    echo "go-server容器存在"
+    echo "go-server容器存在，開始關閉舊容器"
     docker stop go-server
     docker rm go-server
-else
-    docker build -t demo . --no-cache
-    docker run -p 8088:8088 --name go-server -d demo
 fi
+
+docker build -t demo . --no-cache
+docker run -p 8088:8088 --name go-server -d demo
