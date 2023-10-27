b3b402a2ebc666ba895991c15ba4005544b0b2ac
ArZhi <zhi711825@gmail.com>
2023-10-27 11:03:21 +0800

    update port

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 // 2023/09/22-4
 func main() {
 	http.HandleFunc("/", util.SayHello)
-	if err := http.ListenAndServe(":8088", nil); err != nil {
+	if err := http.ListenAndServe(":12003", nil); err != nil {
 		panic(err)
 	}
 }
